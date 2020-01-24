eb443b3e3a322956bebb1fa8c22c9e238177ec1b
Lyell Read <lyellread@gmail.com>
2020-01-24 11:11:52 -0800

    Zipping programs for submission

---
+++ homework/hw2/homework2-programs/Mergesort3.py
@@ -0,0 +1,147 @@
+#!/usr/bin/python3
+
+# Merge Sort 3 Program
+# Lyell Read / 1/23/2020
+
+
+import sys, os
+
+
+def merge3(a, b, c, o):
+	#iterate until we have all empty lists
+	while len(a) > 0 or len(b) > 0 or len(c) > 0:
+		#if any two lists are empty, pop from the one non empty
+		if len(a) == 0 and len(b) == 0:
+			#only c remains, pop to o
+			o.append(c[0])
+			c.remove(c[0])
+
+		#if any two lists are empty, pop from the one non empty
+		elif len(a) == 0 and len(c) == 0:
+			#only b remains, pop to o
+			o.append(b[0])
+			b.remove(b[0])
+
+		#if any two lists are empty, pop from the one non empty
+		elif len(b) == 0 and len(c) == 0:
+			#only a remains, pop to o
+			o.append(a[0])
+			a.remove(a[0])
+
+		else:
+			#at least two lists are full:
+			#if empty list is a, then compare c and b and pop the lower of the two
+			if len(a) == 0:
+				if b[0] > c[0]:
+					o.append(c[0])
+					c.remove(c[0])
+				else:
+					o.append(b[0])
+					b.remove(b[0])
+
+			#if emoty list is b, compare a and c and pop lower
+			elif len(b) == 0:
+				if c[0] > a[0]:
+					o.append(a[0])
+					a.remove(a[0])
+				else:
+					o.append(c[0])
+					c.remove(c[0])
+
+			#if empty list is c, pop from min of a, b
+			elif len(c) == 0:
+				if a[0] > b[0]:
+					o.append(b[0])
+					b.remove(b[0])
+				else:
+					o.append(a[0])
+					a.remove(a[0])
+
+			else:
+				#now we have all three lists full by PoE
+				#assemble the first elements into an array
+				t = [a[0], b[0], c[0]]
+
+				#find the min of that array
+				p = min(t)
+
+				#if the min is the first element of a, choose to pop from a
+				if p == a[0]:
+					o.append(a[0])
+					a.remove(a[0])
+
+				#elif its the first element of b, pop from b
+				elif p == b[0]:
+					o.append(b[0])
+					b.remove(b[0])
+
+				#now we know it must be from c, so pop from c
+				else:
+					o.append(c[0])
+					c.remove(c[0])
+	
+	#return our completed list
+	return o
+
+def mergesort3(x):
+	if len(x) > 1:
+		#generate midpoints of the initial list
+		#left one will be first third
+		ls = len(x)//3
+		#right one will be second third (or half of what
+		# is left after third is removed)
+		rs = ls + (len(x)-ls)//2
+
+		#split the list up into a(1), b(2) and c(3) thirds
+		a = x[:ls]
+		b = x[ls:rs]
+		c = x[rs:]
+
+		#recursively call mergesort on those three
+		a = mergesort3(a)
+		b = mergesort3(b)
+		c = mergesort3(c)
+
+		#merge a b and c to form x.
+		o = []
+		x = merge3(a, b, c, o)
+
+	return x
+
+
+def printarray (a):
+	for e in a:
+		print(e, end=" ")
+	print()
+
+if __name__ == "__main__":
+
+	#arg len
+	if len(sys.argv) < 2:
+		print ("Insufficient Arguments Provided. Quitting.")
+		exit()
+
+	#file exists check
+	if not os.path.exists(sys.argv[1]):
+		print ("File Provided Does Not Exist. Quitting.")
+		exit()
+
+	#open, read, close file
+	with open(sys.argv[1], "r") as f:
+		lines = [ [int(y) for y in x.replace('\n', '').split(" ")][1:] for x in f.readlines()]
+		f.close()
+
+	#generate results list
+	result = [mergesort3(x) for x in lines]
+
+	#print result
+	# for x in result:
+	# 	printarray(x)
+
+	#save results
+	with open("merge.txt", "w") as f:
+		for x in result:
+			f.write(' '.join([str(z) for z in x]) + "\n")
+
+	exit()
+
