c2145c9cc4dc32bd562db819e2f285fd26c689c6
Lyell Read <lyellread@gmail.com>
2020-02-20 01:10:42 -0800

    Adding some parsing pieces

--- homework/hw4/mstEuclid.py
+++ homework/hw4/mstEuclid.py
@@ -4,7 +4,7 @@
 # Lyell Read / 2/5/2020
 
 
-import sys, os, math
+import sys, os, math, itertools
 
 def distancebetween (a, b):
 
@@ -16,7 +16,31 @@ def printarray (a):
     print()
 
 
+def run(filename):
+
+	with open(filename, "r") as f:
+		lines = f.read().splitlines()[1:-1]
+	f.close()
+
+	coords = [[int(y) for y in x.split(" ")] for x in lines]
+
+	edges = [list (x) for x in list(itertools.combinations(coords, 2))]
+
+	printarray(edges)
+
+
 if __name__ == "__main__":
 
-	
+	#arg len
+	if len(sys.argv) < 2:
+		print ("Insufficient Arguments Provided. Quitting.")
+		exit()
+
+	#file exists check
+	if not os.path.exists(sys.argv[1]):
+		print ("File Provided Does Not Exist. Quitting.")
+		exit()
+
+	#start the program!
+	run(sys.argv[1])
 	
